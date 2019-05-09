297a668e357acc85eb62953d654ca30c0bd524f0
WolfWings <wolfwings@gmail.com>
2019-05-09 12:29:47 -0500

    📇Forgot to add the new function to the header file

--- fm_decoder.h
+++ fm_decoder.h
@@ -5,5 +5,6 @@
 
 void fm_decoder_process( uint8_t *input, int32_t len, uint16_t *output );
 void fm_decoder_reset( void );
+void fm_decoder_volume( uint16_t factor ); // 4096 is 100% safe, higher can clip
 
 #endif
